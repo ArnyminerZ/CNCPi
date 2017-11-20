3575d91fe56e853e94e5415c95a9e3dfd9f32b19
ArnyminerZ <Arnyminer.Z@gmail.com>
2017-11-20 20:13:09 +0100

    Safe delete cncpiupdate for upgrading

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # this should be in /temp/install/CNCPi
 
+sudo rm /usr/local/bin/cncpiupdate
+
 sudo cp /temp/install/cncpiupdate /usr/local/bin/cncpiupdate
 sudo chmod +x /usr/local/bin/cncpiupdate
 
