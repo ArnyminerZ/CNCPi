bb5343af103b67a29f6d4c347cba249e415c0abd
ArnyminerZ <Arnyminer.Z@gmail.com>
2017-11-21 18:28:05 +0100

    Changed settings sections and size

--- Web/index.php
+++ Web/index.php
@@ -318,16 +318,14 @@ session_start();
     <div class="container">
         <div class="card-panel">
             <h1>Settings</h1>
-            <div class="row">
-                <div class="col s4">
+            <div class="card-panel row">
+                <div class="col s3">
                     <div class="collection">
-                        <a href="#!" class="collection-item">Alvin</a>
-                        <a href="#!" class="collection-item active">Alvin</a>
-                        <a href="#!" class="collection-item">Alvin</a>
-                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="" class="collection-item active">General</a>
+                        <a href="" class="collection-item">About</a>
                     </div>
                 </div>
-                <div class="col s8">
+                <div class="col s9">
 
                 </div>
             </div>
