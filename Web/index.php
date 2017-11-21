0a63f770c586fe3c88cbdb10ccd7e7a286515c17
ArnyminerZ <Arnyminer.Z@gmail.com>
2017-11-21 21:10:20 +0100

    Included file from /lang

--- Web/index.php
+++ Web/index.php
@@ -2,51 +2,7 @@
 <?php
 session_start();
 
-define("_TITLE", "CNCPi");
-
-define("_LANGUAGE", "Language");
-
-define("_HOME", "Home");
-define("_CLOUD", "Cloud");
-define("_MESSAGES", "Messages");
-define("_SETTINGS", "Settings");
-
-define("_ADD_MACHINE", "Add Machine");
-define("_MACHINE_NAME", "Machine Name");
-define("_CHOOSE_PORT", "Choose a port");
-define("_CONNECT", "Connect");
-define("_PORT", "Port");
-define("_ANY_MESSAGE", "Any message");
-define("_ETA_SYNTAXIS", "&prp% | ETA: &hrh &mnm");
-define("_GO_TO_MACHINE", "Go to Machine");
-define("_MILLING", "Milling");
-define("_DASHBOARD", "Dashboard");
-define("_NOT_MILLING", "Not milling");
-define("_NO", "No");
-define("_CURRENT_PHP_VERSION", "Current <a href=\"http://php.net/\">PHP</a> version");
-define("_CREATE_FOLDER", "Create Folder");
-define("_UPLOAD_FILE", "Upload File");
-define("_RENAME", "Rename");
-define("_DELETE_FOREVER", "Delete Forever");
-define("_DOWNLOAD_ZIP", "Download ZIP");
-define("_RENAME_FILE", "Rename File");
-define("_DOWNLOAD_FILE", "Download File");
-define("_LOAD_FILE", "Load File");
-define("_GENERAL", "General");
-define("_ABOUT", "About");
-define("_SOFTWARE_VERSIONS", "Software Versions");
-define("_CNCPI_RELEASE_VERSION", "CNCPi Release Version");
-define("_UNKNOWN_CNC", "Unknown CNC");
-define("_CONTROL", "Control");
-define("_TERMINAL", "Terminal");
-define("_MOVE_UP", "Move Up");
-define("_MOVE_DOWN", "Move Down");
-define("_MOVE_LEFT", "Move Left");
-define("_MOVE_RIGHT", "Move Right");
-define("_FILE", "File");
-define("_UPLOAD", "Upload");
-define("_FOLDER_NAME", "Folder Name");
-define("_CREATE", "Create");
+include_once "lang/en.php";
 ?>
 <html>
 <head>
