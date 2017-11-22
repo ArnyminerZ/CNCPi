f6badc797bc0a46141ce134f3b41a3c3b571336b
ArnyminerZ <Arnyminer.Z@gmail.com>
2017-11-22 17:10:26 +0100

    Fixed install script
    It showed an error for updating

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 # this should be in /temp/install/CNCPi
 
-sudo rm /usr/local/bin/cncpiupdate
+file="/usr/local/bin/cncpiupdate"
+if [ -f $file ] ; then
+    rm -rf $file
+fi
 
 sudo cp /temp/install/cncpiupdate /usr/local/bin/cncpiupdate
 sudo chmod +x /usr/local/bin/cncpiupdate
