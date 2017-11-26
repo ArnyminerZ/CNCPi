322e383db64a84ebaeb2f525b16ac438479df5f8
ArnyminerZ <Arnyminer.Z@gmail.com>
2017-11-26 20:15:27 +0100

    Added port detection to SerialTest

---
+++ Web/SerialTest.php
@@ -0,0 +1,48 @@
+<?php
+include 'PhpSerial.php';
+
+$comPort = "/dev/ttyACM0"; //The com port address. This is a debian address
+
+$msg = '';
+
+if (isset($_POST["hi"])) {
+    $comPort = $_POST["port"];
+
+    $serial = new PhpSerial;
+    $serial->deviceSet($comPort);
+    $serial->confBaudRate(9600);
+    $serial->confParity("none");
+    $serial->confCharacterLength(8);
+    $serial->confStopBits(1);
+    $serial->deviceOpen();
+    sleep(2); //Unfortunately this is nessesary, arduino requires a 2 second delay in order to receive the message
+    $serial->sendMessage("Well hello!");
+    $serial->deviceClose();
+    $msg = "You message has been sent! WOHOO!";
+}
+?>
+<html>
+<head>
+    <title>Arduino control</title>
+</head>
+<body>
+<form method="POST">
+    <select name="port">
+        <?php
+        $ports = exec("ls /dev/serial/by-id/");
+
+        foreach (explode(" ", $ports) as $port){
+            // <option value="volvo">Volvo</option>
+            echo '<option value="$port">$port</option>';
+        }
+        ?>
+    </select>
+
+    <input type="submit" value="Send" name="hi">
+</form>
+<br>
+
+<?= $msg ?>
+
+</body>
+</html>
