e84c3c6020f39ebf4ae778af7b8dcfe5dcc4886a
ArnyminerZ <Arnyminer.Z@gmail.com>
2017-12-05 10:09:51 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-<iframe src="Web/index.php" style="width: 100%; height: 100%;" frameborder="0" framespacing="0" scrolling="auto" border="0"></iframe>
-</body>
-</html>
